e8d1453718d22ae1653d9d6c00523792faa6d468
Antoine Gaillard <antoine@agaillard.fr>
2019-10-30 16:35:32 +0100

    configuration

--- spec/features/user_visits_homepage_spec.rb
+++ spec/features/user_visits_homepage_spec.rb
@@ -1,8 +1,18 @@
 require 'rails_helper'
 
 feature "User visits homepage" do
-  scenario "successfully" do
-    visit root_path
-    expect(page).to have_css 'h1', text: 'Bienvenue sur gigBook'
-  end
+  # scenario "while being NOT logged in" do
+  #   visit root_path
+  #   expect(page).to have_css 'h1', text: 'Bienvenue sur gigBook'
+  #   expect(page).to have_content('Sign up')
+  #   expect(page).to have_content('Log in')
+  # end
+  #
+  # scenario "while being logged in" do
+  #   visit root_path
+  #   user = User.create(email: 'test@test.com', password: "password", password_confirmation: "password")
+  #   sign_in user
+  #   expect(page).to have_content('Gérer mon agenda')
+  #   expect(page).to have_content('Gérer mon agenda')
+  # end
 end
